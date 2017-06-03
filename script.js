7c0c833746a1e2eedae3dc2585c0f2d8779e6ddd
Rob Martin <rob.martin.5758@gmail.com>
2017-06-03 18:41:36 -0400

    Unit test

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
 
-
-
-for ( var i=100; i >= 0; i--) {
+for ( var i=100; i >= 1; i--) {
 
 if ( i % 15 === 0) {
   document.write("fizzbuzz");
-  document.write("</br>")
-}
-else {
-    if ( i % 5 === 0) {
+  document.write("</br>");
+} else if ( i % 5 === 0) {
         document.write("buzz");
-        document.write("</br>")
-                      }
-    else {
-        if ( i % 3 === 0) {
+        document.write("</br>");               
+   } else if ( i % 3 === 0) {
             document.write("fizz");
-            document.write("</br>")
-                          }
-        else {
+            document.write("</br>");                   
+        } else {
             document.write(i);
-            document.write("</br>")
-             }
-         }    
-      }     
-
-                                }
- 
-                            }
-
-
+            document.write("</br>");
+        }
+    }    
