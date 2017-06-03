99fbbc5c7c2956f42465af15ede0ee0a7a826f8f
Rob Martin <rob.martin.5758@gmail.com>
2017-06-03 18:26:45 -0400

    initial

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+
+for ( var i=100; i >= 0; i--) {
+
+if ( i % 15 === 0) {
+  document.write("fizzbuzz");
+  document.write("</br>")
+}
+else {
+    if ( i % 5 === 0) {
+        document.write("buzz");
+        document.write("</br>")
+                      }
+    else {
+        if ( i % 3 === 0) {
+            document.write("fizz");
+            document.write("</br>")
+                          }
+        else {
+            document.write(i);
+            document.write("</br>")
+             }
+         }    
+      }     
+
+                                }
+ 
+                            }
+
+
